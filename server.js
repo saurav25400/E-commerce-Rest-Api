58ff71af381217df35903c7174f819575ee33d32
saurav25400 <86851522+saurav25400@users.noreply.github.com>
2024-01-14 09:58:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin","*");
   res.header("Access-Control-Allow-Methods","GET","POST","PUT","PATCH","DELETE");
-  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 })
 
