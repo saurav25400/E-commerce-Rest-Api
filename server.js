7df3ff1d368c2201f6860b94e67a250e581cf8fe
saurav25400 <86851522+saurav25400@users.noreply.github.com>
2024-01-14 10:29:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
 
-app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Methods","GET","POST","PUT","PATCH","DELETE");
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
-})
+});
 
 //swagger docs configuration
 
